1ebe9c95102bc7d09a82167220fed5820e5d93b9
bmorris33 <80070986+bmorris33@users.noreply.github.com>
2021-03-08 15:20:30 -0600

    Add files via upload

---
+++ barrett app1/barrett app1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace barrett_app1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Launch browser to youtube...
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSctvfu0S1_6rGp3h13rWxeCb65CHwhBa6VWlZxjeKJgdbwu7A/viewform?pli=1&pli=1");
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Launch browser to youtube...
+            System.Diagnostics.Process.Start("https://mrwachs.wordpress.com/");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad.exe");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
